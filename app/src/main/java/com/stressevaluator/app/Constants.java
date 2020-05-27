8311a2275bd8553142299ebcc4b548b3f79b90df
Aakankasha Sharma <akkutbgs3x@gmail.com>
2020-05-27 14:58:22 +0530

    Updated Questionnaire's name in Constants.java

--- app/src/main/java/com/stressevaluator/app/Constants.java
+++ app/src/main/java/com/stressevaluator/app/Constants.java
@@ -7,8 +7,8 @@ import java.util.List;
 public final class Constants {
         public static final String baseUrl = "https://self-important-real.000webhostapp.com";
         public static final List<String> questionnaireNames = new ArrayList<>
-                (Arrays.asList("Questionnaire 1",
-                "Questionnaire 2",
-                "Questionnaire 3"));
+                (Arrays.asList("1. BUSARI Stress Scale",
+                "2. Questionnaire of academic stress in higher secondary education",
+                "3. Stress in University Students - Work Living Environment Subscale"));
 
 }
